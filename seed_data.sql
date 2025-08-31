2b5599aae4361ea01c5c38b491148219c05a116c
Jiya Shah <shahjiya2024@gmail.com>
2025-08-31 17:33:50 +0530

    Create seed_data.sql

---
+++ seed_data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE faq (
+  id INTEGER PRIMARY KEY,
+  question TEXT,
+  answer TEXT
+);
+
+INSERT INTO faq (question, answer) VALUES
+("Where is the library?", "Library is on 2nd floor, Block B"),
+("How to register exam?", "Use student portal.");
